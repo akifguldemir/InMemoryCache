8bf92e406f3b0ec765bd40ab567e97f527f9e00e
akifguldemir <akif.guldemir@ozyer.com>
2025-04-04 22:30:28 +0300

    object save as json and byte to distrubed cache

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -14,18 +16,46 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         public async Task<IActionResult> Index()
         {
             DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
-            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(10);
 
-            _distributedCache.SetString("name", "asya", options); 
-            await _distributedCache.SetStringAsync("surname", "guldemir", options);
+            //Basic datas
+            //_distributedCache.SetString("name", "asya", options); 
+            //await _distributedCache.SetStringAsync("surname", "guldemir", options);
+
+            Product product = new Product()
+            {
+                Id = 1,
+                Name = "Product 1",
+                Price = 100
+            };
+
+            //as json
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            //as byte
+            Byte[] jsonProductByte = System.Text.Encoding.UTF8.GetBytes(jsonProduct);
+
+            await _distributedCache.SetAsync("product:1", jsonProductByte, options);
+
+            await _distributedCache.SetStringAsync("product:2", jsonProduct, options);
             return View();
         }
 
         public IActionResult Show()
         {
 
-            string name = _distributedCache.GetString("name");
-            ViewBag.name = name;
+            //string name = _distributedCache.GetString("name");
+            //ViewBag.name = name;
+
+            string jsonProduct = _distributedCache.GetString("product:1");
+
+            Byte[] jsonProductByte = _distributedCache.Get("product:1");
+
+            string jsonProductByteString = System.Text.Encoding.UTF8.GetString(jsonProductByte);
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.product = p;
+
             return View();
 
         }
