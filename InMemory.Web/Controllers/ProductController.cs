dd78b2d2c2a364e9930e420567f74111cac00976
akifguldemir <akif.guldemir@ozyer.com>
2025-04-04 15:45:29 +0300

    Add project files.

---
+++ InMemory.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _cache;
+        public ProductController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            _cache.Set<string>("zaman", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            ViewBag.zaman = _cache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
