e0bdc63b66cd2580ecd16dbfac4bbc7e6a147b7e
akifguldemir <akif.guldemir@ozyer.com>
2025-04-04 16:25:10 +0300

    SlidingExpiration and AbsoluteExpiration added

--- InMemory.Web/Controllers/ProductController.cs
+++ InMemory.Web/Controllers/ProductController.cs
@@ -13,13 +13,45 @@ namespace InMemory.Web.Controllers
 
         public IActionResult Index()
         {
-            _cache.Set<string>("zaman", DateTime.Now.ToString());
+            //1.yol
+            //if(String.IsNullOrEmpty(_cache.Get<string>("zaman")))
+            //{
+            //    _cache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
+            //2.yol
+            if(!_cache.TryGetValue("zaman", out string zamancache))
+            {
+                //Cache süresi
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+                //options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); AbsoluteExpiration örneği
+
+                // SlidingExpiration örneği
+                //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+                //SlidingExpiration kullanıclacağında best practices olarak AbsoluteExpiration da verilmeli..1 dk ye kadar veri alınınca 10 sn eklenir
+                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+                _cache.Set<string>("zaman", DateTime.Now.ToString(), options);
+            }
+
             return View();
         }
 
         public IActionResult Get()
         {
-            ViewBag.zaman = _cache.Get<string>("zaman");
+
+            //_cache.Remove("zaman"); Remove
+            //_cache.GetOrCreate("zaman", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //}); yoksa olusturur
+
+            _cache.TryGetValue("zaman", out string zamancache);
+
+            ViewBag.zaman = zamancache;
             return View();
         }
     }
