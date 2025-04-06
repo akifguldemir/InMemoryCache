d75412d4c2bb4112c0252e270d73ddf5e6a4f482
Akif Güldemir <akifguldemir@gmail.com>
2025-04-06 16:57:48 +0300

    StringTypeController added

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            db.StringSet("name", "John Doe");
+            db.StringSet("age", 30);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //Clidaki aynı metotlar
+
+            var value = db.StringGet("name");
+
+            //db.StringIncrement("age", 1);
+
+            //Dönen sonucla ilgili işlem varsa, async
+            //var age = db.StringIncrementAsync("age",10).Result;
+
+            //Sonuca gerek yok async
+            db.StringIncrementAsync("age", 10).Wait();
+
+            //var age = db.StringDecrementAsync("age", 10).Result;
+            if (value.HasValue) ViewBag.Name = value.ToString();
+
+            return View();
+        }
+    }
+}
--- RedisExchangeAPI.Web/Services/RedisService.cs
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -7,21 +7,20 @@ namespace RedisExchangeAPI.Web.Services
         private ConnectionMultiplexer _redis;
         private readonly string _redisHost;
         private readonly string _redisPort;
+        private readonly string _redisConnectionString;
 
         private IDatabase db { get; set;  }
 
         public RedisService(IConfiguration configuration)
         {
-            var _redisHost = configuration["Redis:Host"];
-            var _redisPort = configuration["Redis:Port"];
-
+            _redisConnectionString = configuration["Redis:Configuration"];
         }
 
         public void Connect()
         {
-            var redisConnectionString = $"{_redisHost}:{_redisPort}";
+            var redisConnectionString = $"{_redisConnectionString}";
 
-            _redis = ConnectionMultiplexer.Connect(redisConnectionString);
+            _redis = ConnectionMultiplexer.Connect(_redisConnectionString);
         }
 
         public IDatabase GetDb(int db)
